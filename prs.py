da9a434f9866e0352db3b2c531d4f8ca334c784d
Nikita Maximov <44504716+nikr0zzz@users.noreply.github.com>
2020-12-23 23:02:41 +0300

    Update prs.py

--- prs.py
+++ prs.py
@@ -3,6 +3,7 @@ import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import os
 
 filter_films = {}
 films_content = {}
@@ -45,7 +46,12 @@ def get_url_filter():
 
 
 def get_urls_from_filter(url):
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(url)
     time.sleep(2)
     filter_page = driver.page_source
